4b95347e470c3999a0f4d02a08780ff19150535a
Michał <michal.burda3@gmail.com>
2017-03-02 19:33:09 +0100

    new project files

---
+++ gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css':'sass/style.sass'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['sass/style.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false
+        },
+      } 
+    },
+	
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            'css/style.css',
+            '*.html',
+            '*.js'
+          ]
+        },
+        options: {
+          watchTask: true, 
+          server: './'
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
+
+};
